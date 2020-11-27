91b5c8b21bf0cd66d80b983325fdb52d4a00139f
flynnsbit <32404856+flynnsbit@users.noreply.github.com>
2020-11-27 17:15:17 -0600

    Add EMM warning to Xwing

--- games/STARWARE/1_Start.bat
+++ games/STARWARE/1_Start.bat
@@ -1,5 +1,7 @@
 imgset ide10 "/cd/SWXWCD/swxwcd.cue"
-
+@echo off
+echo.
+echo This game requires EMM386 or QEM.  Please reboot and select the correct memory manager and relaunch the game.
 pause
 cd SWXWCD
 cd XWINGCD
