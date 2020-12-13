15a090fc7051eec1fa2667050f676becf56ceacc
flynnsbit <32404856+flynnsbit@users.noreply.github.com>
2020-12-13 11:20:33 -0600

    Fix for Lemmings, Freddo Changes

--- games/LEMMINGS/lemmings/run.bat
+++ games/LEMMINGS/lemmings/run.bat
@@ -3,36 +3,55 @@
 cls
 echo.
 echo Press 1 for Lemmings w/ Adlib Music
-echo Press 2 for Lemmings w/ CD Music
-echo Press 3 for Lemmings Official Companion Bonus Disk Levels
-echo Press 4 to Quit
+echo Press 2 for Lemmings w/ CD Music (EMM386 Required)
+echo Press 3 for Oh No More Lemmings w/ CD Musc (EMM386 Required)
+echo Press 4 for Lemmings Official Companion Bonus Disk Levels
+echo Press 5 to quit
 echo.
-choice /C:1234 /N Please Choose:
+choice /C:12345 /N Please Choose:
 
-if errorlevel = 4 goto quit
-if errorlevel = 3 goto bonus
+if errorlevel = 5 goto quit
+if errorlevel = 4 goto bonus
+if errorlevel = 3 goto CdOhNo
 if errorlevel = 2 goto CD
 if errorlevel = 1 goto ADLIB
 
 :ADLIB
 del russell.dat
 copy .\russell\russell.dat .\
+attrib +R russell.dat
+sysctl sys L1-
 cls
-call lemmings
-goto menu
+VGALEMMI /v
+sysctl menu
+goto quit
 
-:CD
+:CdOhNo
+imgset ide10 "/cd/lemmings/az_2246.cue"
 cd CDLEMM
 cls
-lemmings
+ohno vga
 cd ..
-goto menu
+goto quit
 
-:bonus
-a:
+:CD
+imgset ide10 "/cd/lemmings/az_2246.cue"
+cd CDLEMM
 cls
 lemmings vga
-goto menu
+cd ..
+goto quit
+
+:bonus
+imgset fdd0 "/floppy/lemmings/lemming.img"
+a:
+sysctl sys L1-
+cls
+call lemmings vga
+e:
+imgset fdd0 ""
+sysctl menu
+goto quit
 
 :quit
 exit
