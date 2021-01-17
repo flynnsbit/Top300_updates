a0e682245200cf967401cf97033b158e690f10cc
flynnsbit <32404856+flynnsbit@users.noreply.github.com>
2021-01-16 19:07:26 -0600

    Note for OneMustFall and L1/L2 bug

--- games/ONEMUSTF/1_Start.bat
+++ games/ONEMUSTF/1_Start.bat
@@ -1,5 +1,5 @@
 imgset ide10 "/cd/OneMustF/omf2097.iso"
-
+Echo At the Player selection screen, turn off either L1 or L2 cache, let it load, and then you can turn it back on.
 @jchoice
 cd OneMustF
 call run
