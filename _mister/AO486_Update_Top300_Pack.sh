f922ce6e4b2426d22269fbd0630dce191c56b866
flynnsbit <32404856+flynnsbit@users.noreply.github.com>
2021-02-08 16:45:52 -0600

    Updates to the update script to clean up bad previous update left overs and also push updates to C

--- _mister/AO486_Update_Top300_Pack.sh
+++ _mister/AO486_Update_Top300_Pack.sh
@@ -14,8 +14,9 @@ base_path="/media/fat"
 # ========= END USER OPTIONS =========
 
 github_repo="flynnsbit/Top300_updates"
-disk_image="IDE 0-1 Top 300 DOS Games.vhd"
+secondary_disk_image="IDE 0-1 Top 300 DOS Games.vhd"
 mount_dir=/tmp/dos_games
+primary_disk_image="IDE 0-0 BOOT-DOS98.vhd"
 
 # Ansi color code variables
 red="\e[0;91m"
@@ -52,29 +53,54 @@ function pause(){
 # Arg $1: Path to image
 # Arg $2: Partition number 
 # Arg $3: Mount point
-mount_image()
+mount_simage()
 {
 	# Get next free loop device
-	loop_dev=$(losetup -f)
+	loop_dev_s=$(losetup -f)
 
 	# Associate next free loop device with image
 	losetup -f "${1}"
 
 	# Mount the partition
-	mount "${loop_dev}p${2}" "${3}"
+	mount "${loop_dev_s}p${2}" "${3}"
 }
 
-# Arg $1: Mount point
-unmount_image()
+mount_pimage()
+{
+	# Get next free loop device
+	loop_dev_p=$(losetup -f)
+
+	# Associate next free loop device with image
+	losetup -f "${1}"
+
+	# Mount the partition
+	mount "${loop_dev_p}p${2}" "${3}"
+}
+
+
+
+# Arg $1: Secondary Mount point
+unmount_simage()
 {
 	# Unmount partition
 	umount "${1}"
 	
 	# Disassociate loop device
-	losetup -d "${loop_dev}"
+	losetup -d "${loop_dev_s}"
 }
 
-find_disk_image()
+# Arg $1: Primary Mount point
+unmount_pimage()
+{
+	# Unmount partition
+	umount "${1}"
+	
+	# Disassociate loop device
+	losetup -d "${loop_dev_p}"
+}
+
+
+find_secondary_disk_image()
 {
 	# Similar logic to MiSTer update.sh script
 	if [ "${games_subdir}" == "" ]; then
@@ -86,36 +112,86 @@ find_disk_image()
 	fi
 
 	ao486_dir="${games_subdir}/AO486"
-	disk_image="${ao486_dir}/${disk_image}"
+	secondary_disk_image="${ao486_dir}/${secondary_disk_image}"
 	
-	if [ ! -f "${disk_image}" ]; then
+	if [ ! -f "${secondary_disk_image}" ]; then
 		echo "Couldn't find disk image in \"${ao486_dir}\"."
 		exit 1
 	fi
 }
-#pause for user
+
+find_primary_disk_image()
+{
+	# Similar logic to MiSTer update.sh script
+	if [ "${games_subdir}" == "" ]; then
+		if [ "$(find ${base_path}/games -type f -print -quit 2> /dev/null)" == "" ] && [ "$(find ${base_path}/AO486 -type f -print -quit 2> /dev/null)" != "" ]; then
+			games_subdir="${base_path}"
+		else
+			games_subdir="${base_path}/games"
+		fi
+	fi
+
+	ao486_dir="${games_subdir}/AO486"
+	primary_disk_image="${ao486_dir}/${primary_disk_image}"
+	
+	if [ ! -f "${primary_disk_image}" ]; then
+		echo "Couldn't find disk image in \"${ao486_dir}\"."
+		exit 1
+	fi
+}
+#Cleaning up any bad updates or failed updates before running again
+echo -en "\ec"
+echo ""
+echo -e "${blue}Cleaning up any previous failed updates, ignore any errors...${reset}"
+echo ""
+set +e
+rm /tmp/update.zip
+unmount_simage "${mount_dir}/E"
+unmount_pimage "${mount_dir}/C"
+rm -r "${mount_dir}"
+set -e
+echo ""
+echo -e "${white}Press any key to continue update . . .${reset}"
+read -s -n 1 -p ""
+
+#pause
 pause
 
+
+
 # Look for disk image in user's games directory
-find_disk_image
+find_secondary_disk_image
 echo ""
-echo -e "${white}Disk image found at \"${disk_image}\".${reset}"
+echo -e "${white}Disk image found at \"${secondary_disk_image}\".${reset}"
 echo ""
+
+find_primary_disk_image
+echo ""
+echo -e "${white}Disk image found at \"${primary_disk_image}\".${reset}"
+echo ""
+
+
+
 # Download latest release zip
 get_latest_release "${github_repo}"
 
 
-# Just mount partition 2 in the disk image
+# Just mount partition 2 for secondary and p1 for primary in the disk image
 mkdir "${mount_dir}"
 mkdir "${mount_dir}/E"
-mount_image "${disk_image}" 2 "${mount_dir}/E"
+mkdir "${mount_dir}/C"
+echo "${secondary_disk_image}"
+echo "${primary_disk_image}"
+mount_simage "${secondary_disk_image}" 2 "${mount_dir}/E"
+mount_pimage "${primary_disk_image}" 1 "${mount_dir}/C"
 echo ""
 # Extract update, overwriting existing files
 unzip -o /tmp/update.zip -d "${mount_dir}/"
 echo ""
 # Clean up
 rm /tmp/update.zip
-unmount_image "${mount_dir}/E"
+unmount_simage "${mount_dir}/E"
+unmount_pimage "${mount_dir}/C"
 rm -r "${mount_dir}"
 echo ""
 echo -e "${green}Successfully updated to ${tag_name}!${reset}"
