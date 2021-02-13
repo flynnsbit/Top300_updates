9c2176ce1ea81f04b67368087d8669c1e8c85249
nanner55 <your@email.com>
2021-02-13 07:53:22 -0800

    Add ini file support. Fix ps truncation problem by renaming top-level update script to shorter name. Fix cleanup loop device disassociation.

--- _mister/update/AO486_Update_Top300_updater.sh
+++ _mister/update/AO486_Update_Top300_updater.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 set -e
-ver="v.0.8"
+ver="v.0.9"
 
 # ======== BEGIN USER OPTIONS ========
 
 # Specifies the Games/Programs subdirectory where core specific directories will be placed.
-# games_subdir="" for letting the script choose between /media/fat and /media/fat/games when it exists,
-# otherwise the subdir you prefer (i.e. games_subdir="/Programs").
-games_subdir=""
+# AO486TOP300DIR="" for letting the script choose between /media/fat and /media/fat/games when it exists,
+# otherwise the subdir you prefer (i.e. AO486TOP300DIR="/Programs").
+AO486TOP300DIR=""
 
 # Base directory for all script’s tasks, "/media/fat" for SD root, "/media/usb0" for USB drive root.
-base_path="/media/fat"
+BASE_PATH="/media/fat"
+
+# Overwrite user variables with values from optional ini file
+source_ini()
+{
+	original_script_path="$0"
+	if [ $original_script_path == "bash" ]
+	then
+		original_script_path=$(ps -o pid,comm=COMMAND_NO_TRUNCATE | awk -v PPID=${PPID} '$1 == PPID {print $2}')
+	fi
+	ini_path=${original_script_path%.*}.ini
+	if [[ -f "${ini_path}" ]] ; then
+		tmp=$(mktemp)
+		dos2unix < "${ini_path}" 2> /dev/null | grep -v "^exit" > ${tmp} || true
+		set +u
+		source ${tmp}
+		set -u
+		rm -f ${tmp}
+	fi
+}
+source_ini
 
 # ========= END USER OPTIONS =========
 
@@ -80,38 +100,46 @@ mount_pimage()
 # Arg $1: Secondary Mount point
 unmount_simage()
 {
+	loop_dev_s=$(losetup -a | awk -F: -v IMG="${1}" '{gsub(/^[\t ]+[0-9]+[\t ]+/,"",$2); if($2 == IMG) { print $1; }}')
+
 	# Unmount partition
 	sync
-	umount "${1}"
+	umount "${2}"
 	
 	# Disassociate loop device
-	losetup -d "${loop_dev_s}"
+	if [ ! -z "$loop_dev_s" -a "$loop_dev_s" != " " ]; then
+		losetup -d "${loop_dev_s}"
+	fi
 }
 
 # Arg $1: Primary Mount point
 unmount_pimage()
 {
+	loop_dev_p=$(losetup -a | awk -F: -v IMG="${1}" '{gsub(/^[\t ]+[0-9]+[\t ]+/,"",$2); if($2 == IMG) { print $1; }}')
+
 	# Unmount partition
 	sync
-	umount "${1}"
+	umount "${2}"
 	
 	# Disassociate loop device
-	losetup -d "${loop_dev_p}"
+	if [ ! -z "$loop_dev_p" -a "$loop_dev_p" != " " ]; then
+		losetup -d "${loop_dev_p}"
+	fi
 }
 
 
 find_secondary_disk_image()
 {
 	# Similar logic to MiSTer update.sh script
-	if [ "${games_subdir}" == "" ]; then
-		if [ "$(find ${base_path}/games -type f -print -quit 2> /dev/null)" == "" ] && [ "$(find ${base_path}/AO486 -type f -print -quit 2> /dev/null)" != "" ]; then
-			games_subdir="${base_path}"
+	ao486_dir="${AO486TOP300DIR}/AO486"
+	if [ "${AO486TOP300DIR}" == "" ]; then
+		if [ "$(find ${BASE_PATH}/games -type f -print -quit 2> /dev/null)" == "" ] && [ "$(find ${BASE_PATH}/AO486 -type f -print -quit 2> /dev/null)" != "" ]; then
+			ao486_dir="${BASE_PATH}/AO486"
 		else
-			games_subdir="${base_path}/games"
+			ao486_dir="${BASE_PATH}/games/AO486"
 		fi
 	fi
 
-	ao486_dir="${games_subdir}/AO486"
 	secondary_disk_image="${ao486_dir}/${secondary_disk_image}"
 	
 	if [ ! -f "${secondary_disk_image}" ]; then
@@ -123,15 +151,15 @@ find_secondary_disk_image()
 find_primary_disk_image()
 {
 	# Similar logic to MiSTer update.sh script
-	if [ "${games_subdir}" == "" ]; then
-		if [ "$(find ${base_path}/games -type f -print -quit 2> /dev/null)" == "" ] && [ "$(find ${base_path}/AO486 -type f -print -quit 2> /dev/null)" != "" ]; then
-			games_subdir="${base_path}"
+	ao486_dir="${AO486TOP300DIR}/AO486"
+	if [ "${AO486TOP300DIR}" == "" ]; then
+		if [ "$(find ${BASE_PATH}/games -type f -print -quit 2> /dev/null)" == "" ] && [ "$(find ${BASE_PATH}/AO486 -type f -print -quit 2> /dev/null)" != "" ]; then
+			ao486_dir="${BASE_PATH}/AO486"
 		else
-			games_subdir="${base_path}/games"
+			ao486_dir="${BASE_PATH}/games/AO486"
 		fi
 	fi
 
-	ao486_dir="${games_subdir}/AO486"
 	primary_disk_image="${ao486_dir}/${primary_disk_image}"
 	
 	if [ ! -f "${primary_disk_image}" ]; then
@@ -139,24 +167,6 @@ find_primary_disk_image()
 		exit 1
 	fi
 }
-
-#Cleaning up any bad updates or failed updates before running again
-echo -en "\ec"
-echo ""
-echo -e "${blue}Cleaning up any previous failed updates, ignore any errors...${reset}"
-echo ""
-#get rid of this hacky cleanup or dont show users the error when the command could not complete
-set +e
-rm /tmp/update.zip 2>/dev/null
-unmount_simage "${mount_dir}/E" 2>/dev/null
-unmount_pimage "${mount_dir}/C" 2>/dev/null
-rm -r "${mount_dir}" 2>/dev/null
-rm -r "${extract_dir}" 2>/dev/null
-echo ""
-echo -e "${white}Hit any key to continue...${reset}"
-read -p ""
-set -e
-
 
 # Look for disk image in user's games directory
 find_secondary_disk_image
@@ -168,6 +178,23 @@ find_primary_disk_image
 echo ""
 echo -e "${white}Disk image found at \"${primary_disk_image}\".${reset}"
 echo ""
+
+#Cleaning up any bad updates or failed updates before running again
+echo -en "\ec"
+echo ""
+echo -e "${blue}Cleaning up any previous failed updates, ignore any errors...${reset}"
+echo ""
+#get rid of this hacky cleanup or dont show users the error when the command could not complete
+set +e
+rm /tmp/update.zip 2>/dev/null
+unmount_simage "${secondary_disk_image}" "${mount_dir}/E" 2>/dev/null
+unmount_pimage "${primary_disk_image}" "${mount_dir}/C" 2>/dev/null
+rm -r "${mount_dir}" 2>/dev/null
+rm -r "${extract_dir}" 2>/dev/null
+echo ""
+echo -e "${white}Hit any key to continue...${reset}"
+read -p ""
+set -e
 
 # Download latest release zip
 get_latest_release "${fastdoom_repo}"
@@ -202,8 +229,8 @@ echo ""
 rm /tmp/update.zip
 rm /tmp/FastDoom_0.7.zip
 sync
-unmount_simage "${mount_dir}/E"
-unmount_pimage "${mount_dir}/C"
+unmount_simage "${secondary_disk_image}" "${mount_dir}/E"
+unmount_pimage "${primary_disk_image}" "${mount_dir}/C"
 rm -r "${mount_dir}"
 rm -r "${extract_dir}"
 rm -r "${fastdoom_dir}"
