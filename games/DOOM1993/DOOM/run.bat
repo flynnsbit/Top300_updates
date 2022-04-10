fd145b017d08aaa5631c3157156b13bc535e9b7a
flynnsbit <32404856+flynnsbit@users.noreply.github.com>
2022-04-10 13:39:41 -0500

    Fix Doom, remove GUS option

--- games/DOOM1993/DOOM/run.bat
+++ games/DOOM1993/DOOM/run.bat
@@ -2,30 +2,17 @@
 echo off
 cls
 echo.
-echo Press 1 for Doom w/ Gravis Ultrasound
-echo Press 2 for Doom w/ SoundBlaster
-echo Press 3 for Doom w/ Sound Canvas
-echo Press 4 to play Network Multiplayer
-echo Press 5 to Quit
+echo Press 1 for Doom w/ SoundBlaster
+echo Press 2 for Doom w/ Sound Canvas
+echo Press 3 to play Network Multiplayer (future)
+echo Press 4 to Quit
 echo.
-jchoice /C:12345 /N Please Choose:
+jchoice /C:1234 /N Please Choose:
 
-if errorlevel = 5 goto quit
-if errorlevel = 4 goto network
-if errorlevel = 3 goto SC55
-if errorlevel = 2 goto SB16
-if errorlevel = 1 goto GUS
-
-:GUS
-REM CONFIG -set"mididevice=default"
-del DEFAULT.CFG
-xcopy .\gus\*.*
-cd ultrasnd
-ultramid
-cd ..
-cls
-DOOM
-goto quit
+if errorlevel = 4 goto quit
+if errorlevel = 3 goto network
+if errorlevel = 2 goto SC55
+if errorlevel = 1 goto SB16
 
 :SB16
 REM CONFIG -set"mididevice=default"
